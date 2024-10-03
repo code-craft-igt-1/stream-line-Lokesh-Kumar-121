ebac69bf8bf9fc072511a1d95dbb66f51bcbacd3
Prince Kumar <Prince.Kumar@Philips.com>
2024-10-03 11:23:21 +0530

    Created Processor for Sender

--- dataSender.cpp
+++ dataSender.cpp
@@ -1,4 +1,5 @@
 #include "./dataSender.h"
+#include <fstream>
 
 // Create a random number generator
 std::mt19937 rng(static_cast<unsigned int>(time(0)));  // Seed with current time
@@ -27,16 +28,22 @@ int generateSpO2() {
     return dist(rng);
 }
 
-// Function to generate and output 50 lines of data to console
+// Function to generate and output 50 lines of data to a file
 void generateData() {
-    // Unit : "Temperature (F), Pulse Rate (bpm), SpO2 (%)";
+    std::ofstream outFile("outputData.txt");
+    if (!outFile) {
+        std::cerr << "Error opening file for writing" << std::endl;
+        return;
+    }
 
+    // Unit : "Temperature (F), Pulse Rate (bpm), SpO2 (%)";
     for (int i = 0; i < 50; ++i) {
         float temperature = generateTemperature();
         int pulseRate = generatePulseRate();
         int spo2 = generateSpO2();
 
-        std::cout << temperature << ", " << pulseRate << ", " << spo2 << "\n";
+        outFile << temperature << ", " << pulseRate << ", " << spo2 << "\n";
     }
-}
 
+    outFile.close();
+}
